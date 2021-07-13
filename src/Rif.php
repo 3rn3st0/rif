b10f6a91e4fce182f087d534b99fda4e3ef0af7d
3rn3st0 <ernestochapon@gmail.com>
2021-07-13 17:42:35 -0400

    Primer commit del paque Rif

---
+++ src/Rif.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rif;
+
+class Rif
+{
+    public function __construct()
+    {
+        $this->test();
+    }
+
+    private function text() : void
+    {
+        echo "Hola, esta es una prueba de la clase.";
+    }
+}
